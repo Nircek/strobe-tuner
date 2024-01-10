8c4cca439ec4d766723894cae57ad90faf49eb37
Marcin Zepp <nircek-2103@protonmail.com>
2024-01-10 17:07:51 +0100

    Ad quant logic and basic animation

--- main.js
+++ main.js
@@ -1,78 +1,198 @@
-const svg = document.getElementsByTagName("svg")[0]; // defer
-const rings = 7;
-const padding = 10;
+const body = document.body;
 
-var log_animation_duration = -10;
+const [, args] = window.location.href.split("?");
+const splittedArgs = (args ?? "7").split("#");
 
-function updateAnimationDuration(animation_duration_db) {
-  const duration = Math.pow(Math.E, -animation_duration_db / 10);
-  svg.style["-webkit-animation-duration"] = `${duration}s`;
-}
+const rings = +splittedArgs[0];
+const quantsPerGroup = +(splittedArgs[1] ?? 3);
+const quantRate = quantsPerGroup << rings;
 
-function changeAnimationDuration(dl) {
-  log_animation_duration += dl;
-  updateAnimationDuration(log_animation_duration);
-}
+/**
+ * TODO: Change frequency of tuning.
+ * @param {number} df
+ */
+function changeFrequency(df) {}
 
-const keydown_handler = {
-  ArrowRight: () => changeAnimationDuration(+1),
-  ArrowLeft: () => changeAnimationDuration(-1),
-  ArrowUp: () => changeAnimationDuration(+1),
-  ArrowDown: () => changeAnimationDuration(-1),
+const keydownHandler = {
+  ArrowRight: () => changeFrequency(+1),
+  ArrowLeft: () => changeFrequency(-1),
+  ArrowUp: () => changeFrequency(+1),
+  ArrowDown: () => changeFrequency(-1),
 };
 
 document.addEventListener("keydown", (e) => {
   const k = e.key;
 
-  if (keydown_handler.hasOwnProperty(k)) {
-    keydown_handler[k](k);
+  if (keydownHandler.hasOwnProperty(k)) {
+    keydownHandler[k](k);
   }
 });
 
-document.addEventListener(
-  "load",
-  (_event) => {
-    for (let i = rings - 1; i >= 0; i--) {
-      for (let j = 0; j < 2 << i; j++) {
-        let angle_part = (2 * Math.PI) / (1 << i);
-        let angle_offset = -Math.PI / 2 + angle_part * (j + 2);
-        arc_fragment_in_center(
-          angle_offset,
-          angle_offset + angle_part / 2,
-          i / rings,
-          (i + 1) / rings
-        );
-      }
-    }
-  },
-  true
+const quantHandlers = Array.from(
+  Array(rings),
+  (_v, ring) => new Array(quantRate >> ring)
 );
 
-function arc_fragment_in_center(
-  start_angle,
-  stop_angle,
-  min_radius,
-  max_radius
-) {
+/**
+ * Divide array into halves and sum element-wise.
+ * @param {number[]} arr an array
+ * @return {number[]}
+ */
+function divideAndSum(arr) {
+  const b = arr.splice(arr.length / 2, arr.length);
+  return arr.map((e, i) => e + b[i]);
+}
+
+/**
+ * Use `quantsValues` to update SVG.
+ * @param {number[]} quantsValues
+ */
+function updateQuants(quantsValues) {
+  console.assert(quantsValues.length == quantRate);
+  const valuesSum = quantsValues.reduce((a, b) => a + b, 0);
+  quantsValues = quantsValues.map((e) => e / valuesSum);
+  for (let ring = 0; ring < rings; ring++) {
+    for (let quant = 0; quant < quantsValues.length; quant++) {
+      quantHandlers[ring][quant].setAttribute("opacity", quantsValues[quant]);
+    }
+    quantsValues = divideAndSum(quantsValues);
+  }
+}
+
+/**
+ * Make only specified quants visible.
+ * @param  {...number} quants
+ */
+function setSingleQuants(...quants) {
+  const aqs = Array.from(new Array(quantRate), () => 0);
+  for (const q of quants) {
+    aqs[q % quantRate] += 1;
+  }
+  updateQuants(aqs);
+}
+
+/**
+ * Generate SVG with quants.
+ */
+function generateQuantSVG() {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  for (let ring = rings - 1; ring >= 0; ring--) {
+    const quantsPerRing = quantRate >> ring;
+    for (let quant = 0; quant < quantsPerRing; ++quant) {
+      const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+      const quantElements = 1 << ring;
+      for (let i = 0; i < quantElements; i++) {
+        const anglePart = (2 * Math.PI) / quantElements;
+        const angleOffset =
+          -Math.PI / 2 + anglePart * (i + quant / quantsPerRing);
+        const arc = arcFragmentInCenter(
+          angleOffset,
+          angleOffset + anglePart / 2,
+          ring / rings,
+          (ring + 1) / rings
+        );
+        group.appendChild(arc);
+      }
+      svg.appendChild(group);
+      quantHandlers[ring][quant] = group;
+    }
+  }
+  svg.setAttribute("viewBox", "0 0 10 10");
+  body.appendChild(svg);
+}
+
+/**
+ * Generate Path with one arc
+ * @param {number} startAngle
+ * @param {number} stopAngle
+ * @param {number} minRadius
+ * @param {number} maxRadius
+ * @return {SVGPathElement}
+ */
+function arcFragmentInCenter(startAngle, stopAngle, minRadius, maxRadius) {
   const arc = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  const mean_angle = (start_angle + stop_angle) / 2;
-  ar = 5 * max_radius;
-  ax1 = 5 + ar * Math.cos(start_angle);
-  ay1 = 5 + ar * Math.sin(start_angle);
-  ax2 = 5 + ar * Math.cos(mean_angle);
-  ay2 = 5 + ar * Math.sin(mean_angle);
-  ax3 = 5 + ar * Math.cos(stop_angle);
-  ay3 = 5 + ar * Math.sin(stop_angle);
-  br = 5 * min_radius;
-  bx1 = 5 + br * Math.cos(start_angle);
-  by1 = 5 + br * Math.sin(start_angle);
-  bx2 = 5 + br * Math.cos(mean_angle);
-  by2 = 5 + br * Math.sin(mean_angle);
-  bx3 = 5 + br * Math.cos(stop_angle);
-  by3 = 5 + br * Math.sin(stop_angle);
+  const meanAngle = (startAngle + stopAngle) / 2;
+  const ar = 5 * maxRadius;
+  const ax1 = 5 + ar * Math.cos(startAngle);
+  const ay1 = 5 + ar * Math.sin(startAngle);
+  const ax2 = 5 + ar * Math.cos(meanAngle);
+  const ay2 = 5 + ar * Math.sin(meanAngle);
+  const ax3 = 5 + ar * Math.cos(stopAngle);
+  const ay3 = 5 + ar * Math.sin(stopAngle);
+  const br = 5 * minRadius;
+  const bx1 = 5 + br * Math.cos(startAngle);
+  const by1 = 5 + br * Math.sin(startAngle);
+  const bx2 = 5 + br * Math.cos(meanAngle);
+  const by2 = 5 + br * Math.sin(meanAngle);
+  const bx3 = 5 + br * Math.cos(stopAngle);
+  const by3 = 5 + br * Math.sin(stopAngle);
   arc.setAttribute(
     "d",
     `M${ax1} ${ay1} A ${ar} ${ar} 0 0 1 ${ax2} ${ay2} A ${ar} ${ar} 0 0 1 ${ax3} ${ay3} L ${bx3} ${by3} A ${br} ${br} 0 0 0 ${bx2} ${by2} A ${br} ${br} 0 0 0 ${bx1} ${by1} L ${ax1} ${ay1}`
   );
-  svg.appendChild(arc);
+  return arc;
 }
+
+let start;
+let previousTimeStamp;
+let animationActive = false;
+
+/**
+ * Animate quant SVG.
+ * @param {DOMHighResTimeStamp} timeStamp
+ */
+function step(timeStamp) {
+  if (!animationActive) {
+    previousTimeStamp = start = undefined;
+    return;
+  }
+  if (start === undefined) {
+    start = timeStamp;
+  }
+  const elapsed = timeStamp - start;
+
+  if (previousTimeStamp !== timeStamp) {
+    const fps = Math.floor(1000 / (timeStamp - previousTimeStamp));
+    fpsHandle.value = `${Number.isFinite(fps) ? fps : 0}`;
+    if ((elapsed / 6000) % 2 < 1) {
+      updateQuants(Array.from(new Array(quantRate), Math.random));
+    } else {
+      const offset = Math.floor(quantRate * ((elapsed % 6000) / 1000) ** 2);
+      const arr = Array.from(
+        new Array(Math.floor((quantRate * (elapsed % 6000)) / 1e5 + 1)),
+        (_, i) => i + offset
+      );
+      setSingleQuants(...arr);
+    }
+  }
+  previousTimeStamp = timeStamp;
+  window.requestAnimationFrame(step);
+}
+
+const fpsHandle = /** @type {HTMLInputElement} */ (
+  document.getElementById("fps")
+);
+
+/** @type {HTMLButtonElement} */ (
+  document.getElementById("start")
+).addEventListener(
+  "click",
+  () => {
+    animationActive = true;
+    window.requestAnimationFrame(step);
+  },
+  true
+);
+/** @type {HTMLButtonElement} */ (
+  document.getElementById("stop")
+).addEventListener(
+  "click",
+  () => {
+    animationActive = false;
+  },
+  true
+);
+
+generateQuantSVG();
+
+setSingleQuants(0);
