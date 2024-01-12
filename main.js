f8978f646dc231a5d64e8098c848a8cf550acf13
Marcin Zepp <nircek-2103@protonmail.com>
2024-01-12 01:19:34 +0100

    Add sliders with parameters of wave synthesis

--- main.js
+++ main.js
@@ -1,18 +1,95 @@
-const body = document.body;
-
 const [, args] = window.location.href.split("?");
-const splittedArgs = (args ?? "7").split("#");
+const ringsArgs = Math.min(Math.max(3, +args), 10);
+if (Number.isFinite(ringsArgs)) {
+  // @ts-ignore
+  document.getElementById("rings").value = ringsArgs;
+}
 
-const rings = +splittedArgs[0];
-const quantsPerGroup = +(splittedArgs[1] ?? 3);
-const quantRate = quantsPerGroup << rings;
+let quantRate = 0;
+let rings;
+let quantHandlers;
+
+let tuneFreq = 220;
+const sampleRate = 48000;
+const soundLength = 20 * sampleRate;
+
+let animationStart;
+let previousAnimationTime;
+let animationActive = false;
+
+generateQuantSVG();
+
+const fpsHandle = /** @type {HTMLInputElement} */ (
+  document.getElementById("fps")
+);
+const playbackHandle = /** @type {HTMLInputElement} */ (
+  document.getElementById("playback")
+);
+
+/** @type {HTMLButtonElement} */ (
+  document.getElementById("start")
+).addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault();
+    animationActive = true;
+    window.requestAnimationFrame(step);
+  },
+  true
+);
+/** @type {HTMLButtonElement} */ (
+  document.getElementById("stop")
+).addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault();
+    animationActive = false;
+  },
+  true
+);
+/** @type {HTMLButtonElement} */ (
+  document.getElementById("apply")
+).addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault();
+    generateQuantSVG();
+    soundArray = generateUserWave();
+  },
+  true
+);
+/** @type {HTMLButtonElement} */ (
+  document.getElementById("clear")
+).addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault();
+    document.getElementsByTagName("form")[0].reset();
+    updateRanges();
+  },
+  true
+);
+
+let soundArray = generateUserWave();
+const ranges = "rings,fund_f,f_radius,f0,f1,f2,f3,f4,f5,f6,f7"
+  .split(",")
+  .map((e) => document.getElementById(e));
+ranges.forEach((e) =>
+  e?.addEventListener("input", (_) => {
+    // @ts-ignore
+    e.nextElementSibling.innerText = e?.value;
+  })
+);
+const updateRanges = () =>
+  ranges.forEach((e) => e?.dispatchEvent(new Event("input")));
+updateRanges();
 
 /**
  * Change frequency of tuning.
  * @param {number} df
  */
 function changeFrequency(df) {
-  refFreq = Math.max(refFreq + df, 0);
+  tuneFreq = Math.max(tuneFreq + df, 0);
 }
 
 const keydownHandler = {
@@ -24,15 +101,15 @@ const keydownHandler = {
 
 document.addEventListener("keydown", (e) => {
   const k = e.key;
+  // @ts-ignore
+  if (e.target?.nodeName == "INPUT") {
+    return;
+  }
 
   if (keydownHandler.hasOwnProperty(k)) {
     keydownHandler[k](k);
   }
 });
-
-const quantHandlers = Array.from(Array(rings), (_v, ring) =>
-  Array(quantRate >> ring)
-);
 
 /**
  * Divide array into halves and sum element-wise.
@@ -76,6 +153,25 @@ function setSingleQuants(...quants) {
  * Generate SVG with quants.
  */
 function generateQuantSVG() {
+  // @ts-ignore
+  const nRings = +document.getElementById("rings").value;
+  if (rings == nRings) return;
+  [...document.getElementsByTagName("svg")].forEach((e) => e.remove());
+  rings = nRings;
+  const quantsPerGroup = {
+    3: 50,
+    4: 30,
+    5: 15,
+    6: 10,
+    7: 5,
+    8: 3,
+    9: 1,
+    10: 1,
+  }[rings];
+  quantRate = quantsPerGroup << rings;
+  quantHandlers = Array.from(Array(rings), (_v, ring) =>
+    Array(quantRate >> ring)
+  );
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   for (let ring = rings - 1; ring >= 0; ring--) {
     const quantsPerRing = quantRate >> ring;
@@ -84,8 +180,7 @@ function generateQuantSVG() {
       const quantElements = 1 << ring;
       for (let i = 0; i < quantElements; i++) {
         const anglePart = (2 * Math.PI) / quantElements;
-        const angleOffset =
-          -Math.PI / 2 + anglePart * (i + quant / quantsPerRing);
+        const angleOffset = anglePart * (i + quant / quantsPerRing);
         const arc = arcFragmentInCenter(
           angleOffset,
           angleOffset + anglePart / 2,
@@ -99,7 +194,10 @@ function generateQuantSVG() {
     }
   }
   svg.setAttribute("viewBox", "0 0 10 10");
-  body.appendChild(svg);
+  document.body.appendChild(svg);
+  if (!animationActive) {
+    setSingleQuants((3 * quantRate) / 4);
+  }
 }
 
 /**
@@ -134,90 +232,50 @@ function arcFragmentInCenter(startAngle, stopAngle, minRadius, maxRadius) {
   return arc;
 }
 
-let start;
-let previousTimeStamp;
-let animationActive = false;
-let offset = 0;
-
 /**
  * Animate quant SVG.
  * @param {DOMHighResTimeStamp} timeStamp
  */
 function step(timeStamp) {
   if (!animationActive) {
-    previousTimeStamp = start = undefined;
+    previousAnimationTime = animationStart = undefined;
     return;
   }
-  if (start === undefined) {
-    previousTimeStamp = start = timeStamp;
+  if (animationStart === undefined) {
+    previousAnimationTime = animationStart = timeStamp;
   }
-  const elapsed = timeStamp - start;
-  const frameDelay = timeStamp - previousTimeStamp;
 
-  if (previousTimeStamp !== timeStamp) {
-    const fps = Math.floor(1000 / (timeStamp - previousTimeStamp));
+  if (previousAnimationTime !== timeStamp) {
+    const fps = Math.floor(1000 / (timeStamp - previousAnimationTime));
     fpsHandle.value = `${Number.isFinite(fps) ? fps : 0}`;
-    const playbackPercent = (elapsed / soundLength) * sampleRate;
-    playbackHandle.value = `${1e3 - Math.abs((playbackPercent % 2e3) - 1e3)}`;
-    const duration = Math.floor((frameDelay / 1000) * sampleRate);
-    const slice = soundArray.slice(offset, offset + duration);
-    offset += duration;
-    if (offset > soundArray.length) offset -= soundArray.length;
+
+    let startSample = Math.floor(
+      ((previousAnimationTime - animationStart) / 1e3) * sampleRate
+    );
+    startSample %= soundLength;
+    let stopSample = Math.floor(
+      ((timeStamp - animationStart) / 1e3) * sampleRate
+    );
+    stopSample %= soundLength;
+    const playbackPercent = (stopSample / soundLength) * 2;
+    if (stopSample < startSample) stopSample += soundLength; // dirty repeating buffer
+
+    playbackHandle.value = `${1e3 * (1 - Math.abs((playbackPercent % 2) - 1))}`;
+
+    const slice = soundArray.slice(startSample, stopSample);
+    const maxInSlice = slice.reduce((a, b) => Math.max(a, Math.abs(b)), 0);
     const quantProbabilities = Array(quantRate).fill(0);
     for (const [i, e] of slice.entries()) {
-      const t = (previousTimeStamp / 1000) * sampleRate + i;
-      const rotationPercent = (t / (sampleRate / refFreq)) % 1;
+      const t = (startSample + i) / sampleRate;
+      const rotationPercent = (t * tuneFreq) % 1;
       const quant = Math.floor(rotationPercent * quantRate);
-      quantProbabilities[quant] += Math.log(Math.abs(e) + 0.01) ** 64;
+      quantProbabilities[quant] += Math.abs(e) / maxInSlice > 0.9999 ? 1 : 0;
     }
     updateQuants(quantProbabilities);
   }
-  previousTimeStamp = timeStamp;
+  previousAnimationTime = timeStamp;
   window.requestAnimationFrame(step);
 }
-
-const fpsHandle = /** @type {HTMLInputElement} */ (
-  document.getElementById("fps")
-);
-const playbackHandle = /** @type {HTMLInputElement} */ (
-  document.getElementById("playback")
-);
-
-/** @type {HTMLButtonElement} */ (
-  document.getElementById("start")
-).addEventListener(
-  "click",
-  () => {
-    animationActive = true;
-    window.requestAnimationFrame(step);
-  },
-  true
-);
-/** @type {HTMLButtonElement} */ (
-  document.getElementById("stop")
-).addEventListener(
-  "click",
-  () => {
-    animationActive = false;
-  },
-  true
-);
-
-generateQuantSVG();
-
-setSingleQuants(0);
-
-let refFreq = 440;
-const sampleRate = 48000;
-const soundLength = 10 * sampleRate;
-const freqRadius = 6;
-const startFreq = 2 * refFreq - freqRadius;
-const stopFreq = 2 * refFreq + freqRadius;
-
-// @ts-ignore
-document.getElementById("start_freq").innerText = `${startFreq} Hz`;
-// @ts-ignore
-document.getElementById("stop_freq").innerText = `${stopFreq} Hz`;
 
 /**
  * Make the array be the array of cumulative sum.
@@ -244,14 +302,59 @@ function makeLinearFrequencySine(start, stop, length) {
   return (i) => Math.sin((2 * Math.PI * freq[i]) / sampleRate);
 }
 
-const freq1 = makeLinearFrequencySine(startFreq, stopFreq, soundLength);
-const freq3 = makeLinearFrequencySine(3 * startFreq, 3 * stopFreq, soundLength);
+/**
+ * Generate wave using user preferences.
+ * @return {number[]}
+ */
+function generateUserWave() {
+  // @ts-ignore
+  const fundFreq = +document.getElementById("fund_f").value;
+  // @ts-ignore
+  const freqRadius = +document.getElementById("f_radius").value;
+  const startFreq = fundFreq - freqRadius;
+  const stopFreq = fundFreq + freqRadius;
 
-const soundArray = Array.from(
-  Array(soundLength),
-  (_, i) => 0.7 * freq1(i) + 0.2 * freq3(i) + 0.1 * Math.random()
-);
-const reverseSoundArray = soundArray.map(
-  (_, i) => soundArray[soundArray.length - 1 - i]
-);
-soundArray.push(...reverseSoundArray);
+  // @ts-ignore
+  document.getElementById("start_freq").innerText = `${startFreq} Hz`;
+  // @ts-ignore
+  document.getElementById("stop_freq").innerText = `${stopFreq} Hz`;
+
+  const freq = Array.from(Array(7), (_, k) =>
+    k == 0
+      ? Math.random
+      : makeLinearFrequencySine(k * startFreq, k * stopFreq, soundLength / 2)
+  );
+
+  let soundArray = Array.from(Array(soundLength / 2), (_, i) =>
+    Array.from(
+      Array(7),
+      (_, k) =>
+        +(
+          // @ts-ignore
+          document.getElementById(`f${k}`).value
+        ) * freq[k](i)
+    ).reduce((a, b) => a + b, 0)
+  );
+  const soundPeak = soundArray.reduce((a, b) => Math.max(a, Math.abs(b)), 0);
+  soundArray = soundArray.map((e) => e / soundPeak);
+
+  const reverseSoundArray = soundArray.map(
+    (_, i) => soundArray[soundArray.length - 1 - i]
+  );
+  soundArray = soundArray.concat(reverseSoundArray);
+  soundArray = soundArray.concat(soundArray);
+
+  const ctx = /** @type {CanvasRenderingContext2D} */ (
+    // @ts-ignore
+    document.getElementById("signal").getContext("2d")
+  );
+  ctx.clearRect(0, 0, 400, 200);
+  ctx.beginPath();
+  const transform = (/** @type {number} */ a) => 100 * (1 - a);
+  ctx.moveTo(0, transform(soundArray[0]));
+  for (const [i, e] of soundArray.entries()) {
+    ctx.lineTo(i, transform(e));
+  }
+  ctx.stroke();
+  return soundArray;
+}
